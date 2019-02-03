0eabc0a64c424d5067945dfe940f2f74f4063b6e
C457 <android.c357@gmail.com>
2019-02-03 11:04:51 -0600

    axon7: Use thermal-engine from fujisan (Axon M)
    * ZTE/P996A26/fujisan:8.1.0/OPM1.171019.026/20180823.144936:user/release-keys
    
    Change-Id: I0a335afa8b0bc7ab1d306effd30777868c365acf

--- device.mk
+++ device.mk
@@ -388,6 +388,10 @@ PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-axon7.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal-engine-axon7-vr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-vr.conf
